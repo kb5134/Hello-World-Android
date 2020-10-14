fa848a28b0f2ccc5ccd7a82904a1787bfc0d4403
allef fressatto <57547119+kb5134@users.noreply.github.com>
2020-10-13 22:48:02 -0300

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.helloworld;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
